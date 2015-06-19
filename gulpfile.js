7fb6ba74ecc8bcbf875e982fafc83641e25f9ea6
Michael Whelan <mjmwdev@gmail.com>
2015-06-19 12:37:48 +0100

    Updated gitignore and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var open = require('gulp-open');
 var deploy = require("gulp-gh-pages");
 
 gulp.task('clean', function () {
-    return gulp.src('dist', {read:false})
+    return gulp.src('Website', {read:false})
         .pipe(clean());
 });
 
@@ -15,7 +15,7 @@ gulp.task('snow', shell.task('.\\Snow\\_compiler\\Snow.exe config=.\\Snow\\'));
 
 gulp.task('connect', function() {
   	connect.server({
-    	root: 'dist/',
+    	root: 'Website/',
     	port: '8123'
   });
 });
@@ -24,20 +24,20 @@ gulp.task("open", function(){
   var options = {
     url: "http://localhost:8123"
   };
-  gulp.src("dist/index.html")
+  gulp.src("Website/index.html")
   .pipe(open("", options));
 });
 
-// generate website to 'dist' folder and then open site in browser
+// generate website to 'Website' folder and then open site in browser
 gulp.task('build', function(callback) {
 	runSequence('clean', 'snow', 'connect', 'open');
 });
 
-// deploy 'dist' folder to mwhelan.github.io github repo, master branch
+// deploy 'Website' folder to mwhelan.github.io github repo, master branch
 var options = { 
 	remoteUrl: "https://github.com/mwhelan/mwhelan.github.io.git",
 	branch: "master"};
   gulp.task('deploy', function () {
-      gulp.src(["dist/**/*.*", "dist/CNAME"])
+      gulp.src(["Website/**/*.*", "Website/CNAME"])
           .pipe(deploy(options));
   });
